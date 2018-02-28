db429c4530e6de4e4c48c808c4e69edb25973f3c
EASV\pgn <stegger.peter@gmail.com>
2018-02-28 14:24:56 +0100

    Added some shizzle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,70 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float WalkSpeed = 1f;
+    public float RunSpeed = 2f;
 
-    public float Speed = 1f;
-
+    private float speed = 0f;
     private Vector2 movementDirection;
     private Rigidbody2D rbody;
+    private Animator animator;
+    private SpriteRenderer _renderer;
 
     // Use this for initialization
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+        speed = WalkSpeed;
     }
 
-     
+
 
     private void FixedUpdate()
     {
-        rbody.velocity = movementDirection * Speed;
+        rbody.velocity = movementDirection;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        dir = dir.normalized;
-        movementDirection = dir;
+        if (Input.GetKeyDown(KeyCode.Q))
+            animator.SetTrigger("Die");
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+            movementDirection = Vector2.zero;
+        }
+        else
+            Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (x < 0f)
+            _renderer.flipX = true;
+        else if (x > 0f)
+            _renderer.flipX = false;
+
+        Vector2 dir = new Vector2(x, y);
+        speed = 0f;
+        if (dir.magnitude > 0f)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = RunSpeed;
+            }
+            else
+            {
+                speed = WalkSpeed;
+            }
+        }
+        movementDirection = dir.normalized * speed;
+        animator.SetFloat("Speed", speed);
     }
 }
